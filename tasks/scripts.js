519f24712dd0080840c3768c958736e86d815201
Roman Zakrevskyi <romul3003@gmail.com>
2018-10-09 00:12:51 +0300

    added babel, works on production

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -2,7 +2,6 @@
 
 const gulp = require('gulp')
 const combine = require('stream-combiner2').obj;
-const uglify = require('gulp-uglify-es').default;
 const $ = require('gulp-load-plugins')();
 
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
@@ -14,9 +13,7 @@ module.exports = function(options) {
             gulp.src(options.src, {since: gulp.lastRun(options.taskName)}),
                 $.if(isDevelopment, $.sourcemaps.init() ),
                 $.concat(options.rename),
-                // uglify({mangle: {toplevel: true}}),
-                $.if(!isDevelopment, combine(uglify({mangle: {toplevel: true}}), $.rev()) ),
-
+                $.if(!isDevelopment, combine($.babel(), $.uglify({mangle: {toplevel: true}}), $.rev()) ),
                 $.if(isDevelopment, $.sourcemaps.write() ),
                 gulp.dest(options.dst),
                 $.if(!isDevelopment, combine($.rev.manifest('scripts.json'), gulp.dest('manifest')))
